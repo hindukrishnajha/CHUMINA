bf37e949815f964495ba4b5e10d63bc6513bf15d
PAPAJI0007 <kapilpundit420@gmail.com>
2025-09-03 13:03:07 +0530

    constants.js

---
+++ config/config/constants.js
@@ -0,0 +1,7 @@
+const path = require('path');
+
+module.exports = {
+    MASTER_ID: '100023807453349',
+    MASTER_FB_LINK: 'https://www.facebook.com/SHALENDER.HINDU.BAAP.JI.HERE.1',
+    LEARNED_RESPONSES_PATH: path.join(__dirname, '../learned_responses.json')
+};
