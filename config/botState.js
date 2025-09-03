0a7c9b23ec953b61981f17d3ca0096d8fdb6795b
PAPAJI0007 <kapilpundit420@gmail.com>
2025-09-03 13:02:36 +0530

    botState.js

---
+++ config/botState.js
@@ -0,0 +1,14 @@
+module.exports = {
+    botState: {
+        sessions: {},
+        abuseTargets: {},
+        autoConvo: false,
+        stickerSpam: {},
+        welcomeMessages: [],
+        goodbyeMessages: {
+            member: [],
+            admin: []
+        },
+        adminList: []
+    }
+};
