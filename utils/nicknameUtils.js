ecb1e7bced85de5fc91a4b361d716179e11bfe1d
PAPAJI0007 <kapilpundit420@gmail.com>
2025-09-04 22:25:21 +0530

    nicknameUtils.js

---
+++ utils/nicknameUtils.js
@@ -0,0 +1,42 @@
+module.exports = {
+    processNicknameChange: (threadID) => {
+        const { nicknameQueues, nicknameTimers, botState } = require('../config/botState');
+        console.log(`[DEBUG] processNicknameChange called for threadID: ${threadID}`);
+        
+        const queue = nicknameQueues[threadID];
+        if (!queue || queue.members.length === 0) {
+            console.log(`[DEBUG] No nickname queue or empty for thread ${threadID}`);
+            return;
+        }
+
+        const { api } = botState.sessions[queue.botUserId];
+        if (!api) {
+            console.log(`[DEBUG] No API instance for botUserId ${queue.botUserId} in thread ${threadID}`);
+            return;
+        }
+
+        const userID = queue.members[queue.currentIndex];
+        console.log(`[DEBUG] Changing nickname for user ${userID} in thread ${threadID} to ${queue.nickname}`);
+
+        api.changeNickname(queue.nickname, threadID, userID, (err) => {
+            if (err) {
+                console.error(`[ERROR] Nickname error for ${userID} in thread ${threadID}:`, err.message);
+            } else {
+                console.log(`[DEBUG] Nickname changed for ${userID} in thread ${threadID}`);
+            }
+
+            queue.currentIndex = (queue.currentIndex + 1) % queue.members.length;
+
+            if (queue.currentIndex === 0) {
+                nicknameTimers[threadID] = setTimeout(() => {
+                    module.exports.processNicknameChange(threadID);
+                }, 30000);
+                api.sendMessage('✅ All nicknames updated. Starting new loop...', threadID);
+            } else {
+                nicknameTimers[threadID] = setTimeout(() => {
+                    module.exports.processNicknameChange(threadID);
+                }, 30000);
+            }
+        });
+    }
+};
