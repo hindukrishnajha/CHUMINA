272048161b042e294f1dbeec896ea4437daab446
PAPAJI0007 <kapilpundit420@gmail.com>
2025-09-06 01:59:02 +0530

    mastercommand.js

--- commands/master/mastercommand.js
+++ commands/master/mastercommand.js
@@ -1,6 +1,12 @@
 module.exports = {
-    handleMasterCommand: (api, threadID, prefix = '#') => {
-        const masterText = `
+  name: "mastercommand",
+  execute(api, threadID, args, event, botState, isMaster) {
+    if (!isMaster) {
+      api.sendMessage('🚫 केवल मास्टर (Shalender Hindu Ji) इस कमांड को यूज कर सकते हैं।', threadID);
+      return;
+    }
+    const prefix = botState.sessions[event.threadID]?.prefix || '#';
+    const masterText = `
 👑 �_M𝗮𝘀𝘁𝗲𝗿 𝗖𝗼𝗺𝗺𝗮𝗻𝗱𝘀 (Only for Shalender Hindu Ji)
 ━━━━━━━━━━━━━━━━━━━━
 • ${prefix}stopall - Stop all bots
@@ -12,6 +18,6 @@ module.exports = {
 • ${prefix}listadmins - List all admins
 ━━━━━━━━━━━━━━━━━━━━
 👑 𝗖𝗿𝗲𝗮𝗧𝗲𝗱 𝗕𝗬: ✶♡⤾➝SHALENDER X..⤹✶➺🪿🫨🩷🪽`;
-        api.sendMessage(masterText, threadID);
-    }
+    api.sendMessage(masterText, threadID);
+  }
 };
