caf2db33ebe47a9eaba974e40b70e7731d68c96f
PAPAJI0007 <kapilpundit420@gmail.com>
2025-09-06 01:12:14 +0530

    botConfig.js

--- config/botConfig.js
+++ config/botConfig.js
@@ -1,9 +1,10 @@
+// ./config/botConfig.js
 module.exports = {
-    botConfig: {
-        prefix: '#',
-        adminID: process.env.ADMIN_ID || '',
-        autoSpamAccept: false,
-        autoMessageAccept: false,
-        antiOut: false
-    }
+  botConfig: {
+    prefix: '#',
+    adminID: process.env.ADMIN_ID || '',
+    autoSpamAccept: false,
+    autoMessageAccept: false,
+    antiOut: true // डिफॉल्ट में ऑन करने के लिए
+  }
 };
