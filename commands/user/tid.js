4b02975849e18532613a3bf1d431ba425c9989d5
PAPAJI0007 <kapilpundit420@gmail.com>
2025-09-06 01:39:06 +0530

    tid.js

--- commands/user/tid.js
+++ commands/user/tid.js
@@ -1,13 +1,14 @@
 module.exports = {
-    handleTid: (api, threadID) => {
-        try {
-            api.getThreadInfo(threadID, (err, info) => {
-                if (err || !info) return api.sendMessage('Failed to get group info.', threadID);
-                api.sendMessage(`📌 Group Name: ${info.threadName || 'N/A'}\n🆔 Group ID: ${threadID}`, threadID);
-            });
-        } catch (e) {
-            api.sendMessage('Error in tid.', threadID);
-            console.error('Tid error:', e);
-        }
+  name: "tid",
+  execute(api, threadID) {
+    try {
+      api.getThreadInfo(threadID, (err, info) => {
+        if (err || !info) return api.sendMessage('Failed to get group info.', threadID);
+        api.sendMessage(`📌 Group Name: ${info.threadName || 'N/A'}\n🆔 Group ID: ${threadID}`, threadID);
+      });
+    } catch (e) {
+      api.sendMessage('Error in tid.', threadID);
+      console.error('Tid error:', e);
     }
+  }
 };
