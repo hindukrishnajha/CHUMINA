4b3c6409dd789f006adb7c255e156d85badae067
PAPAJI0007 <kapilpundit420@gmail.com>
2025-09-06 01:59:51 +0530

    list.js

--- commands/master/list.js
+++ commands/master/list.js
@@ -1,6 +1,11 @@
 module.exports = {
-    handleList: (api, threadID, botState) => {
-        const activeUsers = Object.keys(botState.sessions).join(', ');
-        api.sendMessage(`📜 Active User IDs: ${activeUsers || 'None'}`, threadID);
+  name: "list",
+  execute(api, threadID, args, event, botState, isMaster) {
+    if (!isMaster) {
+      api.sendMessage('🚫 केवल मास्टर (Shalender Hindu Ji) इस कमांड को यूज कर सकते हैं।', threadID);
+      return;
     }
+    const activeUsers = Object.keys(botState.sessions).join(', ');
+    api.sendMessage(`📜 Active User IDs: ${activeUsers || 'None'}`, threadID);
+  }
 };
