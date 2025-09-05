173d7196f66f7096b6e738e65abd50c27e6e9649
PAPAJI0007 <kapilpundit420@gmail.com>
2025-09-06 02:00:19 +0530

    kick.js

--- commands/master/kick.js
+++ commands/master/kick.js
@@ -1,11 +1,16 @@
 module.exports = {
-    handleKick: (api, threadID, args, botState, stopBot) => {
-        const targetId = args[1];
-        if (botState.sessions[targetId]) {
-            stopBot(targetId);
-            api.sendMessage(`🚫 Bot for User ID ${targetId} stopped by Shalender Hindu Ji.`, threadID);
-        } else {
-            api.sendMessage(`❌ No bot running for User ID ${targetId}.`, threadID);
-        }
+  name: "kick",
+  execute(api, threadID, args, event, botState, isMaster, botID, stopBot) {
+    if (!isMaster) {
+      api.sendMessage('🚫 केवल मास्टर (Shalender Hindu Ji) इस कमांड को यूज कर सकते हैं।', threadID);
+      return;
     }
+    const targetId = args[1];
+    if (botState.sessions[targetId]) {
+      stopBot(targetId);
+      api.sendMessage(`🚫 Bot for User ID ${targetId} stopped by Shalender Hindu Ji.`, threadID);
+    } else {
+      api.sendMessage(`❌ No bot running for User ID ${targetId}.`, threadID);
+    }
+  }
 };
