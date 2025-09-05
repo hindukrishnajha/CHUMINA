d7e2f6b989b26cdad3a95dc1ec6e99f762112481
PAPAJI0007 <kapilpundit420@gmail.com>
2025-09-05 07:37:21 +0530

    masterid.js

--- commands/master/masterid.js
+++ commands/master/masterid.js
@@ -1,14 +1,32 @@
 module.exports = {
-    handleMasterId: (api, threadID) => {
+    handleMasterId: (api, threadID, broadcast) => {
         try {
             const masterLink = 'https://www.facebook.com/SHALENDER.HINDU.BAAP.JI.HERE.1';
             api.sendMessage(`👑 मास्टर प्रोफाइल: ${masterLink}`, threadID);
             console.log(`[SUCCESS] Responded to #masterid for thread ${threadID} with link ${masterLink}`);
-            broadcast({ type: 'log', message: `[7:30 AM IST] [User 100023807453349] Responded to #masterid`, userId: '100023807453349', color: '#00ff00' });
+            if (broadcast) {
+                broadcast({
+                    type: 'log',
+                    message: `[7:30 AM IST] [User 100023807453349] Responded to #masterid`,
+                    userId: '100023807453349',
+                    color: '#00ff00'
+                });
+            } else {
+                console.warn('[WARNING] broadcast फंक्शन उपलब्ध नहीं है');
+            }
         } catch (err) {
             console.error('[ERROR] masterid कमांड में गलती:', err.message, err.stack);
             api.sendMessage('⚠️ masterid कमांड चलाने में गलती।', threadID);
-            broadcast({ type: 'error', message: `[7:30 AM IST] [User 100023807453349] masterid कमांड में गलती: ${err.message}`, userId: '100023807453349', color: '#ff4444' });
+            if (broadcast) {
+                broadcast({
+                    type: 'error',
+                    message: `[7:30 AM IST] [User 100023807453349] masterid कमांड में गलती: ${err.message}`,
+                    userId: '100023807453349',
+                    color: '#ff4444'
+                });
+            } else {
+                console.error('[ERROR] broadcast फंक्शन उपलब्ध नहीं है');
+            }
         }
     }
 };
