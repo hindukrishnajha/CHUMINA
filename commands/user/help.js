706a0530a46d176957cf986077ccd39221122d98
PAPAJI0007 <kapilpundit420@gmail.com>
2025-09-08 13:19:50 +0530

    help.js

--- commands/user/help.js
+++ commands/user/help.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 module.exports = {
   name: "help",
-  aliases: ["commands"], // अगर कोई #commands यूज करे
+  aliases: ["commands"],
   execute(api, threadID, args, event, botState, isMaster) {
     try {
       // डायनामिकली कमांड्स लोड करें
@@ -26,36 +26,43 @@ module.exports = {
 
       const prefix = botState.sessions[event.threadID]?.prefix || '#';
       const helpText = `
-✨ BOT COMMANDS MENU
-━━━━━━━━━━━
+✨ BOT COMMANDS MENU ✨
+━━━━━━━━━━━━━━━━━━━━
 
 🔒 Admin Commands
-${prefix}groupnamelock on/off <name>
-${prefix}nicknamelock on/off <nick>
-${prefix}antiout on/off
-${prefix}kickout @user
-${prefix}unsend
-${prefix}send sticker start/stop
-${prefix}autospam accept
-${prefix}automessage accept
-${prefix}loder target on @user
-${prefix}loder stop
-${prefix}autoconvo on/off
-${prefix}learn (trigger) {response}
+${prefix}groupnamelock on/off <name> - ग्रुप का नाम लॉक/अनलॉक करें
+${prefix}nicklock on <nickname> - सभी का निकनेम लॉक करें
+${prefix}nicklock on @user <nickname> - यूजर का निकनेम लॉक करें
+${prefix}nicklock off - निकनेम लॉक बंद करें
+${prefix}nicklock off @user - यूजर का निकनेम लॉक हटाएँ
+${prefix}removenick on @everyone - सभी के निकनेम हटाएँ
+${prefix}removenick on @user - यूजर का निकनेम हटाएँ
+${prefix}removenick off - निकनेम हटाना बंद करें
+${prefix}removenick off @user - यूजर का निकनेम हटाना बंद करें
+${prefix}antiout on/off - ग्रुप से निकलने से रोकें
+${prefix}kickout @user - यूजर को ग्रुप से निकालें
+${prefix}unsend - मैसेज अनसेंड करें
+${prefix}send sticker start/stop - स्टिकर भेजना शुरू/बंद करें
+${prefix}autospam accept - ऑटोस्पैम स्वीकार करें
+${prefix}automessage accept - ऑटोमैसेज स्वीकार करें
+${prefix}loder target on @user - लोडर टारगेट सेट करें
+${prefix}loder stop - लोडर बंद करें
+${prefix}autoconvo on/off - ऑटोकन्वो चालू/बंद करें
 
 🆔 User Commands
-${prefix}tid
-${prefix}uid / ${prefix}uid @mention
-${prefix}info @mention
-${prefix}groupinfo
-${prefix}pair - pair random 
-${prefix}music <song>
+${prefix}learn <trigger> {response} - नया ट्रिगर और रिस्पॉन्स सेट करें
+${prefix}tid - थ्रेड ID देखें
+${prefix}uid / ${prefix}uid @mention - यूजर ID देखें
+${prefix}info @mention - यूजर की जानकारी देखें
+${prefix}groupinfo - ग्रुप की जानकारी देखें
+${prefix}pair - रैंडम पेयर बनाएँ
+${prefix}music <song> - गाना चलाएँ
 
 👑 Special Commands
-${prefix}mastercommand
-${prefix}masterid
+${prefix}mastercommand - मास्टर कमांड्स देखें
+${prefix}masterid - मास्टर ID देखें
 
-━━━━━━━━━━━
+━━━━━━━━━━━━━━━━━━━━
 👑 Created By: ✶♡⤾➝SHALENDER..⤹✶➺🐯
       `;
       api.sendMessage(helpText, threadID);
