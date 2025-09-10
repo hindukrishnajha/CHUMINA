f61b39652c4e8f5df148f98a529aa3be77e3c32e
PAPAJI0007 <kapilpundit420@gmail.com>
2025-09-10 13:50:47 +0530

    botState.js

--- config/botState.js
+++ config/botState.js
@@ -1,14 +1,15 @@
 const botState = {
-    sessions: {},
-    nicknameTimers: {},
-    nicknameQueues: {},
-    stickerSpam: {},
-    lockedGroups: {},
-    abuseTargets: {},
-    welcomeMessages: [],
-    adminList: [],
-    autoConvo: false,
-    memberCache: {}
+  sessions: {},
+  nicknameTimers: {},
+  nicknameQueues: {},
+  stickerSpam: {},
+  lockedGroups: {},
+  abuseTargets: {},
+  welcomeMessages: [],
+  adminList: [],
+  autoConvo: false,
+  memberCache: {},
+  commandCooldowns: {} // commandCooldowns को शुरू में जोड़ा
 };
 
 module.exports = { botState };
