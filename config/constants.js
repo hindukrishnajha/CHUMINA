597f93a26df41c1a146daf728af9a1768792815f
hindukrishnajha <rahulmeenakinnar@gmail.com>
2025-09-22 17:05:32 +0530

    constants.js

--- config/constants.js
+++ config/constants.js
@@ -1,7 +1,8 @@
+// config/constants.js
 const path = require('path');
 
 module.exports = {
     MASTER_ID: '100023807453349',
     MASTER_FB_LINK: 'https://www.facebook.com/SHALENDER.HINDU.BAAP.JI.HERE.1',
-    LEARNED_RESPONSES_PATH: path.join(__dirname, '../learnedResponses.json') // Fixed file name
+    LEARNED_RESPONSES_PATH: path.join(__dirname, '../../learnedResponses.json') // Explicit path for Render
 };
