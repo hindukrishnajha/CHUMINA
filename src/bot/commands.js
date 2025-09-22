5b290e55b955f9e7fd96b95ab18a76a469f52a1c
hindukrishnajha <rahulmeenakinnar@gmail.com>
2025-09-22 17:24:25 +0530

    commands.js

--- src/bot/commands.js
+++ src/bot/commands.js
@@ -1,8 +1,9 @@
 // src/bot/commands.js
 const fs = require('fs');
 const path = require('path');
-const { botState } = require('../config/botState');
-const { LEARNED_RESPONSES_PATH } = require('../config/constants');
+const { botState } = require(path.join(__dirname, '../../config/botState'));
+const { LEARNED_RESPONSES_PATH } = require(path.join(__dirname, '../../config/constants'));
+const { sendBotMessage } = require('./message');
 
 function loadCommands() {
   const commands = new Map();
@@ -97,37 +98,4 @@ function executeCommand(api, threadID, messageID, command, cleanArgs, event, isM
   }
 }
 
-function sendBotMessage(api, message, threadID, replyToMessageID = null, mentions = [], callback = null) {
-  const userId = Object.keys(botState.sessions).find(id => botState.sessions[id].api === api);
-  if (botState.sessions[userId]?.safeMode) {
-    console.log(`SAFE MODE: Skipping message to ${threadID}`);
-    if (callback && typeof callback === 'function') callback(null, null);
-    return;
-  }
-  const randomDelay = Math.floor(Math.random() * 1000) + 1000;
-  setTimeout(() => {
-    const msgObj = typeof message === 'string' ? { body: message, mentions } : { ...message, mentions };
-    if (replyToMessageID && replyToMessageID !== undefined) {
-      msgObj.messageReply = { messageID: replyToMessageID };
-    }
-    api.sendMessage(msgObj, threadID, (err, messageInfo) => {
-      if (err) {
-        console.error(`[SEND-ERROR] Failed to send with reply: ${err.message}. Trying without reply...`);
-        const fallbackMsgObj = typeof message === 'string' ? { body: message, mentions } : { ...message, mentions };
-        api.sendMessage(fallbackMsgObj, threadID, (fallbackErr, fallbackInfo) => {
-          if (fallbackErr) {
-            console.error(`[SEND-ERROR] Fallback failed: ${fallbackErr.message}`);
-          } else if (fallbackInfo?.messageID) {
-            messageStore.storeBotMessage(fallbackInfo.messageID, typeof message === 'string' ? message : JSON.stringify(message), threadID, replyToMessageID);
-          }
-          if (callback && typeof callback === 'function') callback(fallbackErr, fallbackInfo);
-        });
-      } else if (messageInfo?.messageID) {
-        messageStore.storeBotMessage(messageInfo.messageID, typeof message === 'string' ? message : JSON.stringify(message), threadID, replyToMessageID);
-      }
-      if (callback && typeof callback === 'function') callback(err, messageInfo);
-    });
-  }, randomDelay);
-}
-
-module.exports = { loadCommands, executeCommand, sendBotMessage };
+module.exports = { loadCommands, executeCommand };
