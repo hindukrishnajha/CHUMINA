d2d1d0797a5b5052f2dafa7389c46ec17c447837
hindukrishnajha <rahulmeenakinnar@gmail.com>
2025-09-22 17:06:17 +0530

    botConfig.js

--- config/botConfig.js
+++ config/botConfig.js
@@ -1,26 +1,19 @@
 // config/botState.js
 const botState = {
-  sessions: {}, // Bot sessions per user
-  learnedResponses: {}, // Learned responses
-  mafiaGames: {}, // Mafia game states
-  adminList: [], // Admin IDs
-  lockedGroups: {}, // Locked group names
-  welcomeMessages: [], // Welcome messages
-  goodbyeMessages: {
-    member: ['{name} चला गया, भाई लोग! 😢'],
-    admin: ['{name} को एडमिन ने निकाला! 😈']
-  },
-  eventProcessed: {}, // Processed events
-  commandCooldowns: {}, // Command cooldowns
-  roastEnabled: {}, // Roast mode per thread
-  roastCooldowns: {}, // Roast cooldowns per user
-  roastTargets: {}, // Roast targets
-  abuseTargets: {}, // Abuse targets
-  mutedUsers: {}, // Muted users
-  deleteNotifyEnabled: {}, // Delete notification settings
-  chatEnabled: {}, // AI chat settings
-  memberCache: {}, // Cache group members
-  autoConvo: false // Auto conversation mode
+  sessions: {},
+  nicknameTimers: {},
+  nicknameQueues: {},
+  stickerSpam: {},
+  lockedGroups: {},
+  abuseTargets: {},
+  welcomeMessages: [],
+  adminList: [],
+  autoConvo: false,
+  memberCache: {},
+  commandCooldowns: {},
+  learnedResponses: {},
+  eventProcessed: {},
+  chatEnabled: {}
 };
 
 module.exports = { botState };
