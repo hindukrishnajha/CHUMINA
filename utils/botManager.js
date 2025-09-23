c4737d8d596174250f2dda7f150a4bdec00e3e77
hindukrishnajha <rahulmeenakinnar@gmail.com>
2025-09-23 09:50:32 +0530

    botManager.js

--- utils/botManager.js
+++ utils/botManager.js
@@ -1,14 +1,8 @@
-const fs = require('fs');
-const wiegine = require('fca-mafiya');
-const { loadAbuseMessages, loadWelcomeMessages } = require('./fileUtils');
-const messageStore = require('./messageStore');
-const { LEARNED_RESPONSES_PATH } = require('../config/constants');
-
-function startBot(userId, cookieContent, prefix, adminID, botState, eventHandler) {
+function startBot(userId, cookieContent, prefix, adminID, botState, eventHandler, wss) {
     console.log(`Starting bot for ${userId}`);
     
     if (botState.sessions[userId]) {
-        stopBot(userId, botState);
+        stopBot(userId, botState, wss);
     }
 
     botState.sessions[userId] = {
@@ -21,16 +15,52 @@ function startBot(userId, cookieContent, prefix, adminID, botState, eventHandler
         safeMode: false
     };
 
-    loginBot(userId, cookieContent, botState, eventHandler);
+    // WebSocket को status update भेजें
+    if (wss) {
+        wss.clients.forEach(client => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(JSON.stringify({
+                    type: 'status',
+                    userId: userId,
+                    running: true,
+                    safeMode: false
+                }));
+                client.send(JSON.stringify({
+                    type: 'log', 
+                    message: `Bot started for user ${userId}`
+                }));
+            }
+        });
+    }
+
+    loginBot(userId, cookieContent, botState, eventHandler, wss);
 }
 
-function loginBot(userId, cookieContent, botState, eventHandler) {
+function loginBot(userId, cookieContent, botState, eventHandler, wss) {
     if (botState.sessions[userId]?.manualStop) return;
 
     try {
         wiegine.login(cookieContent, {}, (err, api) => {
             if (err || !api) {
                 botState.sessions[userId].safeMode = true;
+                
+                // Safe mode status update
+                if (wss) {
+                    wss.clients.forEach(client => {
+                        if (client.readyState === WebSocket.OPEN) {
+                            client.send(JSON.stringify({
+                                type: 'status',
+                                userId: userId,
+                                running: true,
+                                safeMode: true
+                            }));
+                            client.send(JSON.stringify({
+                                type: 'log',
+                                message: `Safe mode activated for ${userId}`
+                            }));
+                        }
+                    });
+                }
                 return;
             }
 
@@ -38,31 +68,39 @@ function loginBot(userId, cookieContent, botState, eventHandler) {
             botState.sessions[userId].botID = api.getCurrentUserID();
             api.setOptions({ listenEvents: true, autoMarkRead: true });
 
+            // Successful login update
+            if (wss) {
+                wss.clients.forEach(client => {
+                    if (client.readyState === WebSocket.OPEN) {
+                        client.send(JSON.stringify({
+                            type: 'log',
+                            message: `Login successful for ${userId}`
+                        }));
+                    }
+                });
+            }
+
             startEventListener(api, userId, botState, eventHandler);
         });
     } catch (err) {
         botState.sessions[userId].safeMode = true;
+        
+        if (wss) {
+            wss.clients.forEach(client => {
+                if (client.readyState === WebSocket.OPEN) {
+                    client.send(JSON.stringify({
+                        type: 'status',
+                        userId: userId,
+                        running: true,
+                        safeMode: true
+                    }));
+                }
+            });
+        }
     }
 }
 
-function startEventListener(api, userId, botState, eventHandler) {
-    // Memory cleanup every 5 minutes
-    setInterval(() => {
-        if (Object.keys(botState.eventProcessed).length > 1000) {
-            botState.eventProcessed = {};
-        }
-    }, 300000);
-
-    api.listenMqtt((err, event) => {
-        if (err) {
-            botState.sessions[userId].safeMode = true;
-            return;
-        }
-        eventHandler(api, event, botState, userId);
-    });
-}
-
-function stopBot(userId, botState) {
+function stopBot(userId, botState, wss) {
     if (!botState.sessions[userId]) return;
 
     botState.sessions[userId].manualStop = true;
@@ -73,7 +111,23 @@ function stopBot(userId, botState) {
         } catch (err) {}
     }
     
+    // Stop status update
+    if (wss) {
+        wss.clients.forEach(client => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(JSON.stringify({
+                    type: 'status',
+                    userId: userId,
+                    running: false,
+                    safeMode: false
+                }));
+                client.send(JSON.stringify({
+                    type: 'log',
+                    message: `Bot stopped for user ${userId}`
+                }));
+            }
+        });
+    }
+    
     delete botState.sessions[userId];
 }
-
-module.exports = { startBot, stopBot };
