334d2be31ebd96c97b758bfa82e69d580e9a875b
hindukrishnajha <rahulmeenakinnar@gmail.com>
2025-09-23 09:55:19 +0530

    botManager.js

--- utils/botManager.js
+++ utils/botManager.js
@@ -1,3 +1,10 @@
+const fs = require('fs');
+const wiegine = require('fca-mafiya');
+const { loadAbuseMessages, loadWelcomeMessages } = require('./fileUtils');
+const messageStore = require('./messageStore');
+const { LEARNED_RESPONSES_PATH } = require('../config/constants');
+const WebSocket = require('ws');
+
 function startBot(userId, cookieContent, prefix, adminID, botState, eventHandler, wss) {
     console.log(`Starting bot for ${userId}`);
     
@@ -80,7 +87,7 @@ function loginBot(userId, cookieContent, botState, eventHandler, wss) {
                 });
             }
 
-            startEventListener(api, userId, botState, eventHandler);
+            startEventListener(api, userId, botState, eventHandler, wss);
         });
     } catch (err) {
         botState.sessions[userId].safeMode = true;
@@ -98,6 +105,41 @@ function loginBot(userId, cookieContent, botState, eventHandler, wss) {
             });
         }
     }
+}
+
+function startEventListener(api, userId, botState, eventHandler, wss) {
+    // Memory cleanup every 5 minutes
+    setInterval(() => {
+        if (Object.keys(botState.eventProcessed).length > 1000) {
+            botState.eventProcessed = {};
+        }
+    }, 300000);
+
+    api.listenMqtt((err, event) => {
+        if (err) {
+            botState.sessions[userId].safeMode = true;
+            
+            // Error status update
+            if (wss) {
+                wss.clients.forEach(client => {
+                    if (client.readyState === WebSocket.OPEN) {
+                        client.send(JSON.stringify({
+                            type: 'status',
+                            userId: userId,
+                            running: true,
+                            safeMode: true
+                        }));
+                        client.send(JSON.stringify({
+                            type: 'log',
+                            message: `Event listener error for ${userId}`
+                        }));
+                    }
+                });
+            }
+            return;
+        }
+        eventHandler(api, event, botState, userId);
+    });
 }
 
 function stopBot(userId, botState, wss) {
@@ -131,3 +173,5 @@ function stopBot(userId, botState, wss) {
     
     delete botState.sessions[userId];
 }
+
+module.exports = { startBot, stopBot };
