e220c28879e70834cfc811b5cebeb40266bb723e
hindukrishnajha <rahulmeenakinnar@gmail.com>
2025-09-27 07:09:06 +0530

    botState.js

--- config/botState.js
+++ config/botState.js
@@ -9,7 +9,7 @@ const botState = {
   lockedGroups: {},
   abuseTargets: {},
   welcomeMessages: [],
-  adminList: ['100023807453349'],
+  adminList: Object.freeze(['100023807453349']), // Immutable
   autoConvo: false,
   memberCache: {},
   commandCooldowns: {},
@@ -19,8 +19,19 @@ const botState = {
 };
 
 const LEARNED_RESPONSES_PATH = path.join(__dirname, '../config/learned_responses.json');
-if (!fs.existsSync(LEARNED_RESPONSES_PATH)) {
-  fs.writeFileSync(LEARNED_RESPONSES_PATH, JSON.stringify({}, null, 2), 'utf8');
+if (fs.existsSync(LEARNED_RESPONSES_PATH)) {
+  try {
+    const data = JSON.parse(fs.readFileSync(LEARNED_RESPONSES_PATH, 'utf8'));
+    botState.learnedResponses = data.learnedResponses || {};
+    if (data.adminList) {
+      botState.adminList = Object.freeze(data.adminList); // Load adminList from JSON
+    }
+    console.log('Loaded botState:', botState);
+  } catch (e) {
+    console.error('Error loading learned_responses.json:', e);
+  }
+} else {
+  fs.writeFileSync(LEARNED_RESPONSES_PATH, JSON.stringify({ adminList: botState.adminList, learnedResponses: {}, chatEnabled: {}, mafiaGames: {} }, null, 2), 'utf8');
 }
 
 module.exports = { botState };
